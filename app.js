2cdaa4272fef02d38f944b8300e20b165881164c
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2025-06-15 10:04:34 +0530

    feat: integrate submit route for handling form submissions

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 const idx = require("./routes/index");
 app.use("/", idx);
 
+const submit_route = require("./routes/submit");
+app.use("/submit", submit_route);
+
+// app.get("/submit", (req, res) => {
+//   //   res.send("Hello Amrita");
+//   res.redirect("/");
+// });
+
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
